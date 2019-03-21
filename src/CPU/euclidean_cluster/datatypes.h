9fa0390c6934f8e9c302dfc6e2b7467a20b5fe0f
xolit <tansa404@hotmail.de>
2019-03-21 18:43:17 +0100

    Partial cleanup of CPU/euclidean_cluster

--- src/CPU/euclidean_cluster/datatypes.h
+++ src/CPU/euclidean_cluster/datatypes.h
@@ -1,38 +1,30 @@
-#ifndef datatypes_h
-#define datatypes_h
+#ifndef DATATYPES_H
+#define DATATYPES_H
 
-// from the task often unordered sets would make
-// more sense, but for the benchmark, we want output
-// to be guaranteed to be always in the same order
-// (for comparison of the results)
 #include <vector>
 
-// just a 3D point (in pcl PointXYZ)
 typedef struct  {
-    float x,y,z;
+	float x,y,z;
 } Point;
 
 typedef struct  {
-    double x,y,z;
+	double x,y,z;
 } PointDouble;
 
 
-// a 2D Point (in CV a Point2f)
 typedef struct {
-    float x,y;
+	float x,y;
 } Point2D;
 
 
 typedef struct {
-    double x,y,z,w;
+	double x,y,z,w;
 } Orientation;
-    
-// point and additional RGB  value
+
 typedef struct {
     float x, y, z;
     uint8_t r,g,b;
 } PointRGB;
-
 
 typedef struct {
     PointDouble position;
@@ -41,40 +33,27 @@ typedef struct {
 } Boundingbox;
 
 typedef struct{
-    Point2D center; //< the rectangle mass center
-    Point2D size;    //< width and height of the rectangle
-    float angle;    //< the rotation angle. When the angle is 0, 90, 180, 270 etc., the rectangle becomes an up-right rectangle.
+    Point2D center; // mass center
+    Point2D size;    // width and height
+    float angle;    //rotation angle
 } RotatedRect;
-    
-// PointCloud<type>
-// need: iterator, add element, add all elements of other point cloud (use vector?)
 
 typedef std::vector<Point> PointCloud;
 typedef std::vector<PointRGB> PointCloudRGB;
     
-//centroid: set of points, push_back
 typedef struct {
     std::vector<PointDouble> points;
 } Centroid;
 
-// BoundingBoxArray basically an array of bounding boxes
 typedef struct {
    std::vector<Boundingbox> boxes;
 } BoundingboxArray;
 
-//pointIndices: vector of int
 typedef struct PointIndices {
     std::vector<int> indices;
 } PointIndices;
 
-
-
-// some CV macros
-#define CMP(a,b) (((a) > (b)) - ((a) < (b)))
-#define SIGN(a) CMP((a),0)
 #define PI 3.1415926535897932384626433832795
-#define MIN(a,b) ((a>b)? (b) : (a))
-#define MAX(a,b) ((a<b)? (b) : (a))
 
 #endif
 
