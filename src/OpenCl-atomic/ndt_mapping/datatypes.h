6e3409e536b9d511e2a01625d936e74337ca4ac1
xolit <tansa404@hotmail.de>
2020-09-09 17:29:57 +0200

    OpenCl-atomic/ndt_mapping: Replaced duplicated code with shared framework - making use of shared framework methods and types - Voxel types might be incompatible with certain flags (per Voxel point cache) - may need to test for such a case

--- src/OpenCl-atomic/ndt_mapping/datatypes.h
+++ src/OpenCl-atomic/ndt_mapping/datatypes.h
@@ -5,59 +5,61 @@
  * Embedded Systems & Applications Group 2018
  * License: Apache 2.0 (see attachached File)
  */
-#ifndef DATATYPES_H
-#define DATATYPES_H
+#ifndef EPHOS_DATATYPES_H
+#define EPHOS_DATATYPES_H
 
-#include <vector>
+#include "common/datatypes_base.h"
 
-// 3d euclidean point with intensity
-typedef struct PointXYZI {
-    float data[4];
-} PointXYZI;
-
-typedef struct Matrix4f {
-  float data[4][4];
-} Matrix4f;
-
-typedef struct Mat33 {
-  double data[3][3];
-} Mat33;
-
-typedef struct Mat66 {
-  double data[6][6];
-} Mat66;
-
-typedef struct Mat36 {
-  double data[3][6];
-} Mat36;
-
-typedef struct Mat186 {
-  double data[18][6];
-} Mat186;
-
-
-typedef struct Vec5 {
-  double data[5];
-} Vec5;
-
-typedef double Vec3[3];
-
-typedef double Vec6[6];
-
-typedef struct Point4d {
-    float x,y,z,i;
-} Point4d;
-
-typedef std::vector<PointXYZI> PointCloudSource;
-typedef PointCloudSource PointCloud;
-
-typedef struct CallbackResult {
-	bool converged;
-	std::vector<Matrix4f> intermediate_transformations;
-	Matrix4f final_transformation;
-	double fitness_score;
-} CallbackResult;
-
+// #include <vector>
+//
+// // 3d euclidean point with intensity
+// typedef struct PointXYZI {
+//     float data[4];
+// } PointXYZI;
+//
+// typedef struct Matrix4f {
+//   float data[4][4];
+// } Matrix4f;
+//
+// typedef struct Mat33 {
+//   double data[3][3];
+// } Mat33;
+//
+// typedef struct Mat66 {
+//   double data[6][6];
+// } Mat66;
+//
+// typedef struct Mat36 {
+//   double data[3][6];
+// } Mat36;
+//
+// typedef struct Mat186 {
+//   double data[18][6];
+// } Mat186;
+//
+//
+// typedef struct Vec5 {
+//   double data[5];
+// } Vec5;
+//
+// typedef double Vec3[3];
+//
+// typedef double Vec6[6];
+//
+// typedef struct Point4d {
+//     float x,y,z,i;
+// } Point4d;
+//
+// typedef std::vector<PointXYZI> PointCloudSource;
+// typedef PointCloudSource PointCloud;
+//
+// typedef struct CallbackResult {
+// 	bool converged;
+// 	std::vector<Matrix4f> intermediate_transformations;
+// 	Matrix4f final_transformation;
+// 	double fitness_score;
+// } CallbackResult;
+//
 typedef struct VoxelMean {
 	double data[3];
 } VoxelMean;
@@ -74,15 +76,15 @@ typedef struct VoxelGridDimension {
 	int data[2];
 } VoxelGridDimension;
 
-typedef struct Voxel {
-	Mat33 invCovariance;
-	VoxelMean mean;
-	int pointListBegin;
-#ifdef EPHOS_KERNEL_VOXEL_POINT_STORAGE
-	int pointStorageLevel;
-	PointXYZI pointStorage[EPHOS_KERNEL_VOXEL_POINT_STORAGE];
-#endif
-} Voxel;
+// typedef struct Voxel {
+// 	Mat33 invCovariance;
+// 	VoxelMean mean;
+// 	int pointListBegin;
+// #ifdef EPHOS_KERNEL_VOXEL_POINT_STORAGE
+// 	int pointStorageLevel;
+// 	PointXYZI pointStorage[EPHOS_KERNEL_VOXEL_POINT_STORAGE];
+// #endif
+// } Voxel;
 
 typedef struct PointVoxel {
 	Mat33 invCovariance;
@@ -105,7 +107,7 @@ typedef struct PackedVoxelGridInfo {
 	PackedVoxelGridCorner maxCorner;
 } PackedVoxelGridInfo;
 
-typedef std::vector<Voxel> VoxelGrid;
+//typedef std::vector<Voxel> VoxelGrid;
 
 #define PI 3.1415926535897932384626433832795
 
